2fde540060bbf73471588d385a342409089c650c
Michal Kopčík <70224036+gumernus@users.noreply.github.com>
2021-12-22 19:46:07 +0100

    Update Article.vue

--- src/components/Article.vue
+++ src/components/Article.vue
@@ -22,12 +22,11 @@ export default defineComponent({
 
 <style scoped>
 .article-box{
- box-shadow: rgba(149, 157, 165, 0.2) 0px 8px 17px;
 border-radius: 20px;
 margin: 50px;
 padding: 30px;
 padding-left: 50px;
-background-color: #f8f9fa;
+background-color: #e9ecef;
 white-space: pre-wrap;
 }
 h3{ 
